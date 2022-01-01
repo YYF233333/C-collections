ba0ac130a8a37b1679c18e3ed61b0767de702f4d
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2022-01-01 15:37:27 +0800

    [DOCS] add string doc

--- export/string.h
+++ export/string.h
@@ -1,3 +1,23 @@
+/*  
+ * an immutable, copy-on-change string
+ *  
+ * attempt to manage memory automatically without too much change in
+ * C string style and API
+ * 
+ * Strings are provided in thin pointer(char *) with read-only permissions,
+ * any attempt to change the contents provided are **UNDEFINED BEHAVIOR**
+ * and may crash the program, please modify string with provided API
+ * 
+ * API which has modify semantics consume the ownership of the string,
+ * old string is dropped and new string provided in return value, the
+ * previous pointer is now dangling, **DO NOT** dereference it.
+ * 
+ * Experimental:
+ *  reference counting
+ *      String_copy API is provided for shallow copy, which increment
+ *      ref count, when consuming the string, ref count will be decremented
+ *      actual drop only happens when ref count == 0
+*/
 #ifndef STRING_H_
 #define STRING_H_
 
